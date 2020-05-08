173005255273d7df9d63f3c8e1a3d786e3f56840
mahzza <mahsa.79mirmiran@gmail.com>
2020-05-08 21:08:32 +0430

    make the dialog that work add project button

--- src/components/popUp.vue
+++ src/components/popUp.vue
@@ -9,12 +9,12 @@
                 <h2 >Add a new project</h2>
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3">
-                    <v-text-field label="title" v-model="title" prepend-icon="folder"></v-text-field>
-                    <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                    <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
                     <v-menu  v-model="menu2"  :close-on-content-click="false"  :nudge-right="40" transition="scale-transition"   offset-y min-width="290px">
                             <template v-slot:activator="{ on }">
-                                <v-text-field v-model="date"   label="Due date" prepend-icon="date_range" readonly v-on="on"></v-text-field>
+                                <v-text-field v-model="date" :rules="inputRules" :value="formattedDate" label="Due date" prepend-icon="date_range" readonly v-on="on"></v-text-field>
                             </template>
                             <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
                     </v-menu>
@@ -32,12 +32,21 @@ export default {
         return{
             title: '',
             content:'',
-            due:''
+            due:'',
+            date: new Date().toISOString().substr(0, 10),
+            menu: false,
+            modal: false,
+            menu2: false,
+            inputRules: [
+                v => v.length >= 3 || 'Minimum lenght is 3 characters'
+            ]
         }
     },
     methods:{
         submit(){
-            console.log(this.title, this.content)
+            if (this.$refs.form.validate()){
+                console.log(this.title, this.content)
+            }
         }
     }
 }
