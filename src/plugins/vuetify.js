0d635e1d81914835e38a04cb79f1cc27070f60bf
mahzza <mahsa.79mirmiran@gmail.com>
2020-05-09 17:59:17 +0430

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,7 +7,5 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  theme: {
-    dark: true,
-  },
-})
+    iconfont: 'md',
+  })
